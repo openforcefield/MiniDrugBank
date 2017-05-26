7311709b467d2b570ee26fc07adafdc579e81608
bannanc <bannanc@uci.edu>
2017-05-26 13:02:04 -0700

    start adding tests

---
+++ MiniDrugBank/tests/test_minidrugbank.py
@@ -0,0 +1,10 @@
+import openforcefield
+from unittest import TestCase
+
+class TestMiniDrugBank(TestCase):
+    def test_minidrugbank:
+        """
+        Test methods used to create minidrugbank
+        """
+        print("I am a test")
+        return
