c1729394f8197946eefd0c7d31fd66685e64bb48
bannanc <bannanc@uci.edu>
2017-06-01 19:20:47 -0700

    real tests added

--- MiniDrugBank/tests/test_minidrugbank.py
+++ MiniDrugBank/tests/test_minidrugbank.py
@@ -1,20 +1,26 @@
 from unittest import TestCase
 from openforcefield.utils import read_molecules
+from openforcefield.typing.engines.smirnoff.forcefield import ForceField
 from openeye import oechem
 
 class TestMiniDrugBank(TestCase):
+    def __init__(self, *args, **kwargs):
+        """
+        initiate and store molecules
+        """
+        TestCase.__init__(self, *args, **kwargs)
+        self.tripos_mols = read_molecules('../MiniDrugBank_tripos.mol2')
+        self.ff_mols = read_molecules('../MiniDrugBank_ff.mol2')
     def test_repeating_molecules(self):
         """
         Test methods used to create minidrugbank
         """
-        ff_mols = read_molecules('../MiniDrugBank_ff.mol2')
-        tripos_mols = read_molecules('../MiniDrugBank_tripos.mol2')
         smiles = set()
         # check for repeating SMILES
-        for idx, ff_mol in enumerate(ff_mols):
+        for idx, ff_mol in enumerate(self.ff_mols):
             # get SMILES information
             ff_smile = oechem.OECreateIsoSmiString(ff_mol)
-            tri_mol = tripos_mols[idx]
+            tri_mol = self.tripos_mols[idx]
             tri_smile = oechem.OECreateIsoSmiString(tri_mol)
 
             # SMILES should be the same for the two force fields
@@ -26,5 +32,60 @@ class TestMiniDrugBank(TestCase):
             # add smiles to the list
             smiles.add(ff_smile)
 
+    def test_atom_types(self):
+        """
+        Check the number of atom types represented hasn't changed
+        """
+        atom_types = {1: [12, set()], 6: [12, set()], 7: [8, set()],
+                8: [5, set()], 9: [1, set()], 15: [1, set()], 16: [5, set()],
+                17: [1, set()], 35: [1, set()], 53: [1, set()]}
+
+        for mol in self.ff_mols:
+            for atom in mol.GetAtoms():
+                # get atomic number
+                n = atom.GetAtomicNum()
+                t = atom.GetType()
+                self.assertTrue(n in atom_types, msg = "Atomic number %i not in original set" % n)
+                atom_types[n][1].add(t)
+
+        # Check that the number of types here match original
+        for n, [count, s] in atom_types.items():
+            self.assertTrue( len(s) == count, msg = "Current set has %i atom types for atomic number %i, there were %i in the original set" % (len(s), n, count))
+
+    def test_pid_types(self):
+        """
+        Check that the number of smirnoff parameter types hasn't changed
+        """
+        pids = {'HarmonicBondGenerator': [73, set()],
+                'HarmonicAngleGenerator': [34, set()],
+                'PeriodicTorsionGenerator': [136, set()],
+                'NonbondedGenerator': [26, set()]}
+
+        ff = ForceField("forcefield/smirnoff99Frosst.ffxml")
+        labels = ff.labelMolecules(self.ff_mols, verbose = False)
+        # loop through labels from smirnoff
+        for force_dict in labels:
+            for force, label_list in force_dict.items():
+                for (indics, pid, smirks) in label_list:
+                    # we don't have current counts on impropers
+                    if pid[0] == 'i':
+                        continue
+                    pids[force][1].add(pid)
+
+        # Check that the number of types here match original
+        for force, [count, s] in pids.items():
+            self.assertTrue( len(s) == count, msg = "Current set has %i types for the force %s, there were %i in the original set" % (len(s), force, count))
+
+
     #TODO
     #def test_3Dcoordinates(self):
+
+    def test_check_hydrogens(self):
+        """
+        Test that explicit hydrogens are already included on all molecules
+        In other words the implicit hydrogen count on all atoms is zero
+        """
+        for mol in self.tripos_mols:
+            for a in mol.GetAtoms():
+                self.assertTrue(a.GetImplicitHCount() == 0, msg = "Found a %i atom with implicit hydrogens" % a.GetAtomicNum())
+
