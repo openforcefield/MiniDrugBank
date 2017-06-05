866cabdfe9ae139a912201812f71eb257c564289
bannanc <bannanc@uci.edu>
2017-06-05 15:49:49 -0700

    reorganizing

--- minidrugbank/filter.py
+++
@@ -1,9 +0,0 @@
-# This is a python script to see if there is something to import travis will stop complaining.
-from openeye import oechem
-from openforcefield.utils import *
-
-def print_smiles(file_name):
-    molecules = read_molecules(file_name)
-    for mol in molecules:
-        print(oechem.OECreateIsoSmiString(mol))
-    return
