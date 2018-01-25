2f4d1357c278ed8a0b6c8f51a39501225fc58c23
bannanc <bannanc@uci.edu>
2018-01-25 10:33:38 -0500

    force --yes on all conda installations

--- devtools/travis-ci/install.sh
+++ devtools/travis-ci/install.sh
@@ -18,7 +18,7 @@ export PIP_ARGS="-U"
 export PATH=$MINICONDA_HOME/bin:$PATH
 conda update --yes conda
 conda install --yes conda-build jinja2 anaconda-client pip 
-conda install pytest
+conda install --yes pytest
 
 # Restore original directory
 popd
